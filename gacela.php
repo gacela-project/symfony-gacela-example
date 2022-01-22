e852aaef75bb32108683f0112d37f27ab37c4e62
Chemaclass <chemaclass@outlook.es>
2022-01-22 17:03:49 +0100

    Lock gacela to 0.11

--- gacela.php
+++ gacela.php
@@ -9,29 +9,27 @@ use App\Shared\Infrastructure\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Gacela\Framework\AbstractConfigGacela;
 
-return static function (): AbstractConfigGacela {
-    return new class() extends AbstractConfigGacela {
-        public function config(): array
-        {
-            return [
-                'type' => 'env',
-                'path' => '.env*',
-                'path_local' => '.env',
-            ];
-        }
+return static fn() => new class() extends AbstractConfigGacela {
+    public function config(): array
+    {
+        return [
+            'type' => 'env',
+            'path' => '.env*',
+            'path_local' => '.env',
+        ];
+    }
 
-        public function mappingInterfaces(array $globalServices): array
-        {
-            /** @var Kernel $kernel */
-            $kernel = $globalServices['symfony/kernel'];
+    public function mappingInterfaces(array $globalServices): array
+    {
+        /** @var Kernel $kernel */
+        $kernel = $globalServices['symfony/kernel'];
 
-            return [
-                ProductRepositoryInterface::class => ProductRepository::class,
-                ProductEntityManagerInterface::class => ProductRepository::class,
-                EntityManagerInterface::class => static fn() => $kernel
-                    ->getContainer()
-                    ->get('doctrine.orm.entity_manager'),
-            ];
-        }
-    };
+        return [
+            ProductRepositoryInterface::class => ProductRepository::class,
+            ProductEntityManagerInterface::class => ProductRepository::class,
+            EntityManagerInterface::class => static fn() => $kernel
+                ->getContainer()
+                ->get('doctrine.orm.entity_manager'),
+        ];
+    }
 };
